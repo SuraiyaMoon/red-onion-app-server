bb29be3f2c985e1a69acd0940cda5fab6523ea64
SuraiyaMoon01 <=>
2022-08-04 20:49:48 +0600

    breakfast route created

--- index.js
+++ index.js
@@ -1,22 +1,36 @@
 const express = require("express");
 const app = express();
 const cors = require("cors")
-const port = 5000 || process.env.PORT;
+require('dotenv').config();
+
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+//middleware
+
+app.use(cors());
+app.use(express.json());
 
 
 
 //mongodb
-
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.SECRET_USER}:${process.env.SECRET_PASS}@cluster0.s7vy7.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 async function run() {
     try {
-        client.connect();
-        console.log('mongo connected')
+        await client.connect();
+
+        const breakfastCollection = client.db("red_onion").collection("breakfast");
+
+        app.get('/breakfast', async (req, res) => {
+            const query = {};
+            const breakfastResult = await breakfastCollection.find(query).toArray();
+            res.send(breakfastResult)
+        })
+
+        console.log('mongo connected with route')
     }
-    catch {
+    finally {
 
     }
 }
